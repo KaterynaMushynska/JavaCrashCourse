5040aea69f8e530abfcd0f205b38bfc0a3c624eb
Kateryna Mushynska <kate080227@gmail.com>
2021-09-13 21:39:24 +0300

    added task Is n divisible by x and y

---
+++ src/test/java/codewars/Conditions/DivisibleNbTest.java
@@ -0,0 +1,18 @@
+package codewars.Conditions;
+
+import com.Conditions.DivisibleNb;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DivisibleNbTest {
+    @Test
+    public void test1() {
+        assertEquals(true, DivisibleNb.isDivisible(12, 4, 3));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals(false, DivisibleNb.isDivisible(3, 3, 4));
+    }
+}
