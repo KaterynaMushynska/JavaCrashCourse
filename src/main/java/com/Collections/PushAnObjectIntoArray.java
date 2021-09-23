dc2efd7dd34a24863299e0474aad00038be8cfe3
Kateryna Mushynska <kate080227@gmail.com>
2021-09-23 14:11:30 +0300

    added task Push a hash/an object into array

---
+++ src/main/java/com/Collections/PushAnObjectIntoArray.java
@@ -0,0 +1,12 @@
+package com.Collections;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PushAnObjectIntoArray {
+    public static List<String> push() {
+        List<String> items = new ArrayList<>();
+        items.add("an object");
+        return items;
+    }
+}
---
+++ src/test/java/codewars/Collections/PushAnObjectIntoArrayTest.java
@@ -0,0 +1,17 @@
+package codewars.Collections;
+
+import com.Collections.PushAnObjectIntoArray;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class PushAnObjectIntoArrayTest {
+    @Test
+    public void testSomething() {
+        List<String> items = PushAnObjectIntoArray.push();
+        assertEquals(1, items.size());
+        assertEquals("an object", items.get(0));
+    }
+}
