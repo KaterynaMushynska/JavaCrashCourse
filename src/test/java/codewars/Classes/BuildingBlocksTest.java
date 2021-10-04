c55e2237f565bafee6c577bbce0d6c11b2fdaaab
Kateryna Mushynska <kate080227@gmail.com>
2021-10-04 10:59:49 +0300

    added task Building blocks

---
+++ src/test/java/codewars/Classes/BuildingBlocksTest.java
@@ -0,0 +1,18 @@
+package codewars.Classes;
+
+import com.Classes.BuildingBlocks;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BuildingBlocksTest {
+    @Test
+    public void testBasicBlock() throws Exception {
+        BuildingBlocks b = new BuildingBlocks(new int[]{2, 2, 2});
+        assertEquals(2, b.getWidth());
+        assertEquals(2, b.getLength());
+        assertEquals(2, b.getHeight());
+        assertEquals(8, b.getVolume());
+        assertEquals(24, b.getSurfaceArea());
+    }
+}
