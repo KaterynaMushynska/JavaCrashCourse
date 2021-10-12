5618e2545b843d4e262a979537c0277f62355b2f
Kateryna Mushynska <kate080227@gmail.com>
2021-10-12 15:20:46 +0300

    added task Interactive Dictionary

---
+++ src/main/java/com/Collections/InteractiveDictionary.java
@@ -0,0 +1,19 @@
+package com.Collections;
+
+import java.util.HashMap;
+
+public class InteractiveDictionary {
+    private HashMap<String, String> map;
+
+    public InteractiveDictionary() {
+        map = new HashMap<>();
+    }
+
+    public void newEntry(String key, String value) {
+        map.put(key, value);
+    }
+
+    public String look(String key) {
+        return map.getOrDefault(key, "Cant find entry for " + key);
+    }
+}
