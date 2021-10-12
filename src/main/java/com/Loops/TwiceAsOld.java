12eb2782048cca9084763730555f729765b0e49b
Kateryna Mushynska <kate080227@gmail.com>
2021-10-12 11:00:48 +0300

    added task Twice as old

---
+++ src/main/java/com/Loops/TwiceAsOld.java
@@ -0,0 +1,7 @@
+package com.Loops;
+
+public class TwiceAsOld {
+    public static int TwiceAsOld(int dadYears, int sonYears) {
+        return (int) Math.abs(dadYears - sonYears * 2);
+    }
+}
---
+++ src/test/java/codewars/Loops/TwiceAsOldTest.java
@@ -0,0 +1,16 @@
+package codewars.Loops;
+
+import com.Loops.TwiceAsOld;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TwiceAsOldTest {
+    @Test
+    public void testSomething() {
+        assertEquals(30, TwiceAsOld.TwiceAsOld(30, 0));
+        assertEquals(16, TwiceAsOld.TwiceAsOld(30, 7));
+        assertEquals(15, TwiceAsOld.TwiceAsOld(45, 30));
+
+    }
+}
